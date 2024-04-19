7fe1e41a4a490f5df92dc86ee874daace2537e70
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-04-19 18:00:40 +0100

    working on the users guess

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+
+function characters(){
 fetch('images.json')
   .then(response => response.json())
   .then(data => {
-    const container = document.getElementById('character-container');
-
     
-      const img = document.createElement('img');
-      setInterval(function() {
-        let random = Math.floor(Math.random() * data.length);
-        console.log(random)
-        img.src = data[random].path;
-        container.appendChild(img);
-        }, 3000);
-       
+    const container = document.getElementById('character-container');
+    const img = document.createElement('img');
+    const input = document.createElement('input');
+    
       
+    let random = Math.floor(Math.random() * data.length);
+  
+    img.src = data[random].path;
+    container.appendChild(img);
+    let source = data[random].name;
+    let [name,type] = source.split('.');
+
+    let nameSpaces = name.replace(/_/g, ' ');
+    console.log(nameSpaces);
+    
+    input.placeholder = 'guess the character';
+    container.appendChild(input);
+    
+      
+     
   })
   .catch(error => console.error('Error:', error));
+}
+  function startGame() {
+
+    console.log('Starting game');
+
+    let startButton = document.querySelector('.start');
+    startButton.style.display = 'none';
+    characters();
+
+  }
+
+  
