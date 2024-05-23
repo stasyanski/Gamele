f30c1361b27b34eb6863a884a7f237c01a5460d1
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-05-23 17:41:11 +0100

    working on sorting the highest to lowest leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -6,8 +6,64 @@ const leaderboard = document.querySelector('.leaderboard ol ');
 
 //adding scores through local storage
 
+// username =prompt('Enter your username');
+// score =prompt('Enter your score');
 
-let leaderboardUser =document.createElement('li');
+function addScore(username, score) {
+    console.log(localStorage.length);
+    
+    
+    // leaderboard.appendChild(leaderboardUser ); // adds the username and score and displays them
+    localStorage.setItem(username, score);
+    
+    // leaderboardUser.textContent = username + ' ' + score;
+    
+}
+// this function si going to get the scores and username from local storage and display them  from highest to lowest
+function displayScore() {
+    let scores = [];
 
-leaderboard.appendChild(leaderboardUser + localStorage); // adds the username and score and displays them
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        console.log(key);
+        let value = localStorage.getItem(key);
+        // scores.push({ name: key, score: value });
+        // console.log(key, value);
+        try{
+            
+            if (isNaN(value)) {
+                throw new Error('Not a number');
+
+            }
+            else{
+                scores.push({ name: key, score: value });
+            }
+            
+            
+        }catch(e){
+        
+            // localStorage.removeItem(key);
+
+
+        }
+
+        
+
+
+
+
+        
+    
+    }
+
+    scores.sort((a, b) => b.score - a.score);
+    console.log(scores);
+    
+    
+}
+
+// addScore(username, score);
+displayScore();
+// addScore(username,score);
+
 
