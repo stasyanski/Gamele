bdbbdab529401ab8971d08fc1b0ba00e0f871481
stas <157709692+stasyanski@users.noreply.github.com>
2024-05-30 18:51:38 +0100

    Converted to ES from CommonJS
    Added notes

--- fetch.js
+++ fetch.js
@@ -1,11 +1,26 @@
-/* the fetch.js file is used to fetch the images.json
- * file which contains all filename and paths of the images
-
-
+/* Fetching images
+ * from a directory (characters)
 */
 
+import { readdir, writeFile } from 'fs';
+import { join } from 'path';
 
-fetch('images.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+const folder_path = 'characters';
+
+readdir(folder_path, (err, files) => {
+    if (err) {
+        console.error('error reading directory:', err);
+        return;
+    }
+
+    const data = files.map(file => ({
+        name: file,
+        path: join(folder_path, file)
+    }));
+
+    writeFile('images.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            console.error('error writing file:', err);
+        }
+    });
+});
