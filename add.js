0e0e63f5116cc80b065dcdcea92d38a381b52145
kambains226 <121152303+kambains226@users.noreply.github.com>
2024-05-30 18:44:36 +0100

    added the add.js file which adds the images to the json file

---
+++ add.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = 'characters';
+
+fs.readdir(folderPath, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    const data = files.map(file => ({
+        name: file,
+        path: path.join(folderPath, file)
+    }));
+
+    fs.writeFile('images.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        }
+    });
+});
--- fetch.js
+++ fetch.js
@@ -1,6 +1,9 @@
 /* the fetch.js file is used to fetch the images.json
  * file which contains all filename and paths of the images
+
+
 */
+
 
 fetch('images.json')
     .then(response => response.json())
