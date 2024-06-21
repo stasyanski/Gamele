6546b08ff54a4e0b6819f23d4186cbee2a541b23
stas <157709692+stasyanski@users.noreply.github.com>
2024-06-21 20:58:56 +0100

    Beautify dom elements

--- script.js
+++ script.js
@@ -7,19 +7,19 @@
  */
 
 // DOM elements
-const container = document.querySelector(".character_container");
-const extra_lives = document.querySelector(".lives");
-const img = document.createElement("img");
-const left_arrow = document.querySelector(".left_arrow");
-const right_arrow = document.querySelector(".right_arrow");
-const enter_button = document.querySelector(".enter_bttn");
-const score = document.querySelector(".score");
+const container             = document.querySelector(".character_container");
+const extra_lives           = document.querySelector(".lives");
+const img                   = document.createElement("img");
+const left_arrow            = document.querySelector(".left_arrow");
+const right_arrow           = document.querySelector(".right_arrow");
+const enter_button          = document.querySelector(".enter_bttn");
+const score                 = document.querySelector(".score");
 const ui_elements_container = document.querySelector(".ui_elements_container");
-const input_container = document.querySelector(".input_container");
-const start_btn = document.querySelector(".start");
-const choices = document.querySelectorAll(".choice");
-const darken_bg_var = document.querySelector(".darken_bg");
-const timer_div = document.querySelector(".timer");
+const input_container       = document.querySelector(".input_container");
+const start_btn             = document.querySelector(".start");
+const choices               = document.querySelectorAll(".choice");
+const darken_bg_var         = document.querySelector(".darken_bg");
+const timer_div             = document.querySelector(".timer");
 
 // game state variables
 let input;
